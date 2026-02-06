9338e2f5b41f6c4965bedbb5814d4d1fec2db2a7
Marcos Francisco Flores Ramos <mfcoflores@gmail.com>
2026-02-06 01:48:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5519165939-247e1",
+  "appId": "1:676186373910:web:ec2a9dae97ca5bb31c5a37",
+  "apiKey": "AIzaSyBne0p6OznGGeL8BEwoNRpoPCz5RDZr6RA",
+  "authDomain": "studio-5519165939-247e1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "676186373910"
+};
